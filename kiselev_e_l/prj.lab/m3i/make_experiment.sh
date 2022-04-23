18e3abe3183bd6dd4df9e484c8b13ee5eb154c8d
Egor Kiselev <kiselev.el@phystech.edu>
2022-04-24 00:14:05 +0300

    Experiment conducted, plotting code added

--- kiselev_e_l/prj.lab/m3i/make_experiment.sh
+++ kiselev_e_l/prj.lab/m3i/make_experiment.sh
@@ -5,13 +5,15 @@
 # 2 - alloc_type
 # 3 - svn link
 
-mkdir experiments
+echo "::::EXPERIMENT FOR $1::::"
+# mkdir experiments
 cd experiments
-svn co https://github.com/egor79k/algorithms_course/trunk/kiselev_e_l
+# svn co https://github.com/egor79k/algorithms_course/trunk/kiselev_e_l
 svn co $3
 cp m3i/m3i.h m3i/m3i.cpp kiselev_e_l/prj.lab/m3i
+rm -r -f m3i
 cd kiselev_e_l
-mkdir build
+# mkdir build
 cd build
 cmake ..
 make m3i_alloc_time
--- kiselev_e_l/prj.lab/m3i/run_exp_for_all.sh
+++ kiselev_e_l/prj.lab/m3i/run_exp_for_all.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# bash make_experiment.sh videneeva_n_v T* https://github.com/VNVid/algo_6sem/trunk/videneeva_n_v/prj.lab/m3i
-# bash make_experiment.sh dron_a_a T* https://github.com/qweqweq1222/algos/trunk/dron_a_a/prj.lab/m3i
-# bash make_experiment.sh kiselev_e_l T* https://github.com/egor79k/algorithms_course/trunk/kiselev_e_l
-# bash make_experiment.sh kutakova_a_p T* https://github.com/ArinaMand/kutakova_a_p.git
+## bash make_experiment.sh videneeva_n_v T* https://github.com/VNVid/algo_6sem/trunk/videneeva_n_v/prj.lab/m3i
+bash make_experiment.sh dron_a_a T* https://github.com/qweqweq1222/algos/trunk/dron_a_a/prj.lab/m3i
+bash make_experiment.sh kiselev_e_l T* https://github.com/egor79k/algorithms_course/trunk/kiselev_e_l/prj.lab/m3i
+bash make_experiment.sh kutakova_a_p T* https://github.com/ArinaMand/kutakova_a_p/trunk/kutakova_a_p/prj.lab/m3i
 bash make_experiment.sh manakov_d_d T* https://github.com/DanilaDanila/mipt-se/trunk/manakov_d_d/prj.lab/m3i
-# bash make_experiment.sh nikitina_p_v T* https://github.com/apollinaria-sleep/algorithms__sem_6/trunk/nikitina_p_v/prj.lab/m3i
-# bash make_experiment.sh orusov_t_d T* https://github.com/orusovten/Algo_CT/trunk/orusov_t_d/prj.lab/m3i
+## bash make_experiment.sh nikitina_p_v T* https://github.com/apollinaria-sleep/algorithms__sem_6/trunk/nikitina_p_v/prj.lab/m3i
+bash make_experiment.sh orusov_t_d T* https://github.com/orusovten/Algo_CT/trunk/orusov_t_d/prj.lab/m3i
 bash make_experiment.sh pavliuchenkov_d_m T* https://github.com/und3f1n3d5/svn_repo/trunk/pavliuchenkov_d_m/prj.lab/m3i
-# bash make_experiment.sh padas_o_a T* https://github.com/ppadas/SE_algo/trunk/padas_o_a/prj.lab/m3i
-# bash make_experiment.sh pozdnyakova_a_a T* https://github.com/alicepozd/algo.git/trunk/pozdnyakova_a_a/prj.lab/m3i
+## bash make_experiment.sh padas_o_a T* https://github.com/ppadas/SE_algo/trunk/padas_o_a/prj.lab/m3i
+## bash make_experiment.sh pozdnyakova_a_a T* https://github.com/alicepozd/algo/trunk/pozdnyakova_a_a/prj.lab/m3i
